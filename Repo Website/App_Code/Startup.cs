aad711a3b1ef1a172549d48a7b583fd63ad93eb6
wwzeng <wzeng3@studentmail.nait.ca>
2014-09-04 08:48:23 -0600

    Intiial Website and Solution

---
+++ Repo Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Repo_Website.Startup))]
+namespace Repo_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
